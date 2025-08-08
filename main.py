435709bcfcf500e682306ea1eea8bc0328bdcbe2
Symbian-Bro <siddhubhai998@gmail.com>
2025-08-08 16:42:56 +0530

    Created the function to capture the primary selection using xclip. Log file will now be saved under a hidden directory(.log_primary) .

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import os
 import subprocess
 
-log_file_path = "~/.log_primary"
+log_file_path = os.path.expanduser("~/.log_primary/log_primary.txt")
+
+def get_primary_selection():
+
+    try:
+        result = subprocess.run(
+            ["xclip", "-o", "-selection", "primary"],
+            capture_output=True,
+            text=True
+        )
+        text = result.stdout.strip()
+        return text
+    except Exception:
+        return ""
