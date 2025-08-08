4f9007392e7bba0bab3d4b7b41304eb1f1438542
Symbian-Bro <siddhubhai998@gmail.com>
2025-08-08 16:55:52 +0530

    Created the main loop. The latest primary selection is now stored in a variable called "latest_selection". Imported the time module and added a sleep for 200ms. The file is opened in ab mode to avoid overwriting.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import subprocess
+import time
 
 log_file_path = os.path.expanduser("~/.log_primary/log_primary.txt")
-
 os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
 
 def get_primary_selection():
@@ -17,3 +17,14 @@ def get_primary_selection():
         return text
     except Exception:
         return ""
+
+last_selection = None
+
+while (True):
+    latest_selection = get_primary_selection()
+    if (latest_selection != last_selection):
+        last_selection = latest_selection
+        with open(log_file_path, "ab") as f:
+            f.write((selection + "\n").encode("utf-8"))
+
+         time.sleep(0.2)
